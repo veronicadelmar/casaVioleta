0c64434c4a2ee81bc8ba2970d4193c0b185ca7fb
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-09-07 21:11:10 -0300

    tailwind css, tailwind ui, react icon were installed and the header component was created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./components/Header"
 function App() {
   return (
     <>
-     
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+import logo from '../assets/logo.svg';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdClose } from 'react-icons/md';
+import { Dialog, Popover } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+const products = [
+  { name: 'Área Escolar'},
+  { name: 'Cultura y Bienestar'},
+  { name: 'Área Digital'},
+  { name: 'Capacitación Laboral'},
+]
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="h-[70px] w-screen px-[25px] flex flex-row flex-wrap justify-between items-center text-[#E5B2FF] bg-[#461e5b] xl:h-[100px] xl:px-[60px]">
+        {/* logo */}
+        <figure>
+          <img className='w-2/4 xl:w-full' src={logo} alt='logo casa violeta' />
+        </figure>
+        {/* navBar */}
+        <nav>
+          {/* btn burger */}
+          <figure className="flex xl:hidden">
+            <button type="button"
+                    onClick={() => setMobileMenuOpen(true)}>
+                <GiHamburgerMenu className='w-[24px] h-[24px]'/>
+            </button>
+          </figure>
+          {/* option's Desktop */}
+          <Popover.Group className="hidden xl:flex xl:gap-x-[50px]">
+            <a href="#" className="xl:text-xl xl:semi-bold">
+                Inicio
+            </a>
+            <Popover className="relative">
+              <Popover.Button className="flex items-center gap-x-1 xl:text-xl xl:semi-bold">
+                Cursos y Talleres
+                <ChevronDownIcon className="h-5 w-5 flex-none" aria-hidden="true" />
+              </Popover.Button>
+                <Popover.Panel className="absolute top-full z-10 mt-6 max-w-md overflow-hidden bg-[#461e5b] w-[261px]">
+                  <div>
+                    {products.map((item) => (
+                      <div
+                        key={item.name}
+                        className="group relative flex items-center"
+                      >
+                        <div className="flex-auto">
+                          <a href={item.href} className="block font-semibold text-[#E5B2FF] p-5 border-[#E5B2FF] border-b-[1px]">
+                            {item.name}
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </Popover.Panel>
+            </Popover>
+            <a href="#" className="xl:text-xl xl:semi-bold">
+              Información de Contacto
+            </a>
+            <a href="#" className="xl:text-xl xl:semi-bold">
+              Ayudanos
+            </a>
+          </Popover.Group>
+      </nav>
+      {/* Mobile */}
+      <Dialog as="div" className="xl:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full bg-[#461e5b] overflow-y-auto">
+        <nav className='h-[70px] px-[25px] flex flex-row flex-wrap justify-between items-center border-[#E5B2FF] border-b-2'>
+            {/* logo */}
+            <figure>
+                <img className='w-2/4' src={logo} alt='logo casa violeta' />
+            </figure>
+            {/* btn close */}
+            <figure className="xl:hidden">
+            <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+            >
+                <MdClose className='w-[30px] h-[30px] text-[#E5B2FF]'/>
+            </button>
+            </figure>
+        </nav>
+        {/* FALTA TAMAÑO DE LETRA MOBILE */}
+            <div className="text-[#E5B2FF]">
+              <div className="flex flex-col">
+                <a href="#" className="p-5 border-[#E5B2FF] border-b-[1px]">Inicio</a>
+                <a href="#" className="p-5 border-[#E5B2FF] border-b-[1px]">Área Escolar</a>
+                <a href="#" className="p-5 border-[#E5B2FF] border-b-[1px]">Área Digital</a>
+                <a href="#" className="p-5 border-[#E5B2FF] border-b-[1px]">Capacitación Laboral</a>
+                <a href="#" className="p-5 border-[#E5B2FF] border-b-[1px]">Información de Contacto</a>
+              </div>
+            </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
