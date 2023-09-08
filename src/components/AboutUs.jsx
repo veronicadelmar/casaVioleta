217dafad901923b4e88d4dc627b9028522b5a969
Rocio-Ch <rociomagali77@gmail.com>
2023-09-08 17:03:22 -0300

    modify styles in AboutUs and Contactform

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,7 +6,7 @@ export default function AboutUs() {
     return (
         <section className='text-[#140819] bg-[#F8EBFF] w-[360px] xl:w-full xl:flex xl:flex-col'>
             <div className='xl:w-[1280px] xl:flex xl:flex-col xl:relative xl:self-center'>
-                <img src={imgVideo} className='w-full mb-[43px] xl:mt-0 xl:mb-0' alt='Video presentación Casa Violeta' />
+                <img src={imgVideo} className='w-full mb-[30px] xl:mt-0 xl:mb-0' alt='Video presentación Casa Violeta' />
                 <div className='hidden xl:flex xl:absolute xl:bottom-[-45px]'>
                     <RiArrowDownDoubleLine size={100} />
                 </div>
@@ -16,13 +16,12 @@ export default function AboutUs() {
             <div className='mx-[18px] xl:flex xl:flex-col xl:mx-0 xl:bg-[#E5B2FF]'>
                 <div className='xl:max-w-[1280px] xl:self-center xl:flex xl:justify-center xl:py-[50px] xl:px-[60px] xl:mb-0'>
                     <div className='xl:mr-[119px]'>
-                        <h3 className='mb-[30px] text-xl font-medium leading-normal xl:mb-0 xl:text-[38px] xl:font-semibold xl:pb-[50px]'>¿Quienes somos?</h3>
+                        <h2 className='mb-[30px] text-xl font-medium leading-normal xl:mb-0 xl:text-[38px] xl:font-semibold xl:pb-[50px]'>¿Quienes somos?</h2>
                         <p className='text-sm font-normal leading-[23px] xl:text-xl xl:leading-[26px] xl:font-medium'>Casa Violeta es una institución que tiene como objetivo potenciar las capacidades de niños, jóvenes y adultos, dotándolos de herramientas formativas para que puedan desarrollarse de forma autónoma.</p>
                         <p className='mb-[30px] text-sm font-normal leading-[23px] xl:mb-[30px] xl:text-xl xl:leading-[26px] xl:font-medium'>Contamos con un equipo interdisiplinario, colaboradores, voluntarios, técnicos y profesionales especialistas en educación, formación y empleo.</p>
                     </div>
                     <img src={imgAbout} className='mb-[30px] xl:mb-0 xl:max-w-[571px]' alt='Casa violeta' />
-                </div>
-            
+                </div>   
             </div>      
         </div>
         </section>
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,29 +2,34 @@ import actionForm from '../assets/actionForm.png'
 
 export default function ContactForm() {
     return (
-        <section className='m-[18px] xl:m-[60px] xl:flex xl:flex-col'>
-            <div>
-                <h3 className='font-medium text-xl py-[30px] xl:text-[38px] xl:font-semibold xl:py-[70px]'>Información de Contacto</h3>
-                <p className='hidden font-normal text-xl leading-[30px] xl:flex'>Para que podamos brindarte toda la  información que necesitás, escribinos sobre qué curso o taller querés iniciar. No te va a llevar más de 5 minutos, y te contestaremos muy pronto.</p>
-                <p className='hidden font-normal text-xl leading-[30px] pb-[30px] xl:flex xl:pb-[70px]'>Una vez que hayas completado los datos, apretá el botón “Enviar” para que nos llegue tu consulta. ¡Y listo!</p>
+        <section className='flex flex-col justify-center w-[360px] text-[#140819] xl:w-full'>
+            <div className='bg-gradient-to-r from-[#E5B2FF] to-[#e5b2ff00] xl:flex xl:flex-col xl:mx-0'>
+                <div className='py-[30px] xl:mx-0 xl:p-[60px] xl:w-[1280px] xl:self-center'>
+                    <h3 className='mx-[18px] font-medium text-xl leading-normal xl:mx-0 xl:text-[44px] xl:font-semibold'>Información de Contacto</h3>
+                </div>
             </div>
-            <div className='xl:flex'>
-                <form action='' className='bg-[#ccc] flex flex-col px-[25px] w-[383px] rounded-lg xl:w-[571px] xl:mr-[29px]'>
-                    <p className='py-[30px] font-semibold text-lg'>¡Envianos tu mensaje aquí abajo!</p>
-                    <label htmlFor='name' className='mb-2 font-medium'>Nombre</label>
+            <div className='py-[30px] mx-[18px] xl:mx-0 xl:py-[50px] xl:p-[60px] xl:w-[1280px] xl:self-center'>
+                <p className='text-sm xl:text-xl xl:font-medium xl:leading-[30px]'>Para que podamos brindarte toda la  información que necesitás, escribinos sobre qué curso o taller querés iniciar. No te va a llevar más de 5 minutos, y te contestaremos muy pronto.</p>
+                <p className='text-sm xl:text-xl xl:font-medium xl:leading-[30px]'>Una vez que hayas completado los datos, apretá el botón <span className='font-bold mx-1.5'>“Enviar”</span> para que nos llegue tu consulta. ¡Y listo!</p>
+            </div>
+            <div className='flex flex-col items-center mb-[30px] xl:items-start xl:flex-row xl:px-[60px] xl:mb-[100px] xl:w-[1280px] xl:self-center'>
+                <form action='' className='bg-[#E5B2FF] w-[324px] px-[25px] rounded-lg flex flex-col xl:w-[571px] xl:h-[631px] xl:mr-[29px]'>
+                    <p className='hidden xl:flex xl:pt-[40px] xl:pb-[50px] xl:font-semibold xl:text-[22px]'>¡Envianos tu mensaje aquí abajo!</p>
+                    <p className='text-lg font-semibold leading-normal py-[30px] xl:hidden'>Consultas</p>
+                    <label htmlFor='name' className='mb-2 font-normal font-medium leading-[23px] xl:text-[22px] xl:leading-normal'>Nombre</label>
                     <input type='text' id='name' placeholder='Tu nombre completo' className='rounded-lg h-[50px] p-5 mb-[30px]' />
 
-                    <label htmlFor='phone' className='mb-2 font-medium'>Teléfono</label>
+                    <label htmlFor='phone' className='mb-2 font-normal font-medium leading-[23px] xl:text-[22px] xl:leading-normal'>Teléfono</label>
                     <input type='text' id='phone' placeholder='Tu número de celular' className='rounded-lg h-[50px] p-5 mb-[30px]' />
 
-                    <label htmlFor='message' className='mb-2 font-medium'>Consulta</label>
+                    <label htmlFor='message' className='mb-2 font-normal font-medium leading-[23px] xl:text-[22px] xl:leading-normal'>Consulta</label>
                     <textarea name='message' id='message' cols='30' rows='10' placeholder='Escribinos tu consulta acerca del curso o taller' className='rounded-lg p-5 h-[190px]'></textarea>
 
                     <div className='flex justify-end'>
-                        <button type='submit' className='my-[30px] text-xl py-2.5 px-5 font-medium leading-normal bg-white rounded-lg'>Enviar</button>
+                        <button type='submit' className='bg-[#004C0B] text-[#F8EBFF] my-[30px] py-2.5 px-5 text-xl font-medium leading-normal rounded-lg xl:text-[22px] xl:py-2.5 xl:px-5 xl:leading-normal'>Enviar</button>
                     </div>
                 </form>
-                <img src={actionForm} alt='Woman' className='hidden xl:flex w-[557px] h-[640px]' />
+                <img src={actionForm} alt='Woman' className='hidden xl:flex xl:w-[571px] h-[640px]' />
             </div>
         </section>
     )
