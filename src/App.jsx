c891667c34c4e9854f590b53b48e0aa5d855d7bb
CarlosMBarboza <techosunion@hotmail.com>
2023-09-11 14:51:50 -0300

    content edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import Location from './components/Location';
 import Footer from './components/footer';
-import SchoolArea from './components/ScholArea';
-import WorkArea from './components/WorkArea';
+
 function App() {
   return (
     <div className="App">
-      
-     
-  {/*<SchoolArea/>
-
-  <WorkArea/>*/}
   <Location/>
    <Footer/>
     </div>
