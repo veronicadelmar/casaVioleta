4e8960e722a74ec85191f48cfa8ea5f059bc6ba7
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-09-12 15:42:39 -0300

    modified component location

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,33 +1,35 @@
-
-
+import { TbMathGreater } from 'react-icons/tb';
 function Location(){ 
     return (
-        <section className="mb_[100px]">
-        <div className=" text-[#E5B2FF] bg-[#543366] text-center py-[50px] text-[38px] ">
-            <h2>Mapa </h2>
-        </div>
-    <div className="flex flex-wrap justify-center items-center  mt-[100px] mx-[6opx] ">
-    <div className="max-w-xl flex flex-col xl:w-[571px] xl:h-[300px]  w-[326] h-[170]  rounded-lg  m-5 shadow-md">
-        
-            <div className="flex flex-col justify-between flex-grow p-5 text-sm  rounded-lg text-[#E5B2FF] bg-[#543366] xl:leading-normal xl:text-lg">
-                <h2 className="">¿Como llegar?</h2>
-                <p className="mt-[30px]"> Estamos ubicados en Álvarez 32, Barrio Don Bosco, 
-                Partido de Quilmes, a dos cuadras de la Estación de Tren. 
-                Algunas formas de llegar:</p>
-                <ol className="">
-                    <li>Tren: Línea Roca (sale desde Constitución, C.A.B.A.)</li>
-                    <li>Líneas de Colectivos:</li>
-                        <ul>
-                            <li>324 (pasa por calle Cramer)</li>
-                            <li> 85A, 98A, 98E (pasa por Av. San Martín)</li>
-                        </ul>
-                </ol> 
-        </div>
-    </div>
-    <iframe className="max-w-xl flex flex-col w-[326] h-[170] xl:w-[571px] xl:h-[300px]  m-5 bg-white border border-gray-200  shadow"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.090665356867!2d-58.29697832525358!3d-34.702893072918826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a3320fb68c51fd%3A0x229bf0f9f60595a!2sAgust%C3%ADn%20%C3%81lvarez%20
-            32%2C%20Don%20Bosco%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1693873326113!5m2!1ses-419!2sar"> </iframe>
-    </div>
-    </section>
+        <section className="w-[360px] xl:w-full">
+            <h2 className="text-[#E5B2FF] bg-[#543366] text-xl font-medium leading-normal text-center py-[28px] mb-[30px] xl:text-[38px] xl:py-[50px] xl:mb-[100px]">Mapa</h2>
+            {/* container */}
+            <div className="mx-[18px] xl:flex xl:mx-[60px]">
+                <div className="text-[#E5B2FF] bg-[#543366] p-[20px] rounded-lg xl:w-[571px] xl:h-[300px]">
+                    <h3 className="font-semibold text-lg mb-[20px] xl:mb-[30px]">¿Como llegar?</h3>
+                    <p className="text-sm font-normal leading-[23px] xl:text-xl xl:leading-[26px] xl:font-medium">Estamos ubicados en Álvarez 32, Barrio Don Bosco, 
+                    Partido de Quilmes, a dos cuadras de la Estación de Tren. 
+                    Algunas formas de llegar:</p>
+                    <ol className='text-sm font-normal'>
+                        <li>Tren: Línea Roca</li>
+                        <li>Líneas de Colectivos:</li>
+                            <ul>
+                                <li className='flex flex-row items-center ml-[8px]'>
+                                    <TbMathGreater className='text-sm mr-[3px]'/>
+                                    324 (pasa por calle Cramer)
+                                </li>
+                                <li className='flex flex-row items-center ml-[8px]'>
+                                    <TbMathGreater className='text-sm mr-[3px]'/>
+                                    85A, 98 1 y 5 (pasa por Av. San Martín)
+                                </li>
+                            </ul>
+                    </ol> 
+                </div>
+                <div className='my-[30px] xl:my-[0]'>
+                    <iframe className='w-full xl:w-[571px] xl:h-[300px]' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.0906642832356!2d-58.2944034!3d-34.702893100000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a3320fb68c51fd%3A0x229bf0f9f60595a!2sAgust%C3%ADn%20%C3%81lvarez%2032%2C%20Don%20Bosco%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1694543230752!5m2!1ses!2sar"></iframe>
+                </div>
+            </div>
+        </section>
     );
 }
 export default Location;
