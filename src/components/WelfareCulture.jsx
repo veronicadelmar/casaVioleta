0dab041579dc43e70646c014fd33cc8e09e22107
Rocio-Ch <rociomagali77@gmail.com>
2023-09-12 15:34:37 -0300

    add link to whatsapp in courses cards

--- src/components/WelfareCulture.jsx
+++ src/components/WelfareCulture.jsx
@@ -7,7 +7,7 @@ import comingSoon from '../assets/rocket-icon-empty-state.svg'
 
 export default function WelfareCulture() {
     return (
-        <section className='culturaBienestar flex flex-col justify-center w-[360px] text-[#140819] xl:w-full'>
+        <section className='culturaBienestar flex flex-col justify-center self-center w-[360px] text-[#140819] xl:w-full'>
             <div className='bg-gradient-to-r from-[#E5B2FF] to-[#e5b2ff00] xl:flex xl:flex-col xl:mx-0'>
                 <div className='py-[30px] xl:mx-0 xl:p-[60px] xl:w-[1280px] xl:self-center'>
                     <h3 className='mx-[18px] font-medium text-xl leading-normal xl:mx-0 xl:text-[44px] xl:font-semibold'>Cultura y Bienestar</h3>
@@ -24,7 +24,7 @@ export default function WelfareCulture() {
                         <p className='px-5 font-medium leading-[25px] xl:text-xl leading-[30px]'>En este taller, los niños tendrán la oportunidad de explorar una variedad de técnicas artísticas como dibujo, pintura y collage, entre otras. Mediante estas actividades, podrán experimentar y dar forma a sus emociones de manera creativa. Todos los materiales necesarios para cada propuesta serán proporcionados por nosotros.</p>
                     </div>
                     <div className='flex justify-end py-7 px-5'>
-                        <span className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</span>
+                        <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</a>
                     </div>
                 </div>
                 <div className='Card bg-[#E5B2FF] w-[324px] mb-[30px] rounded-t-lg flex flex-col shadow-[0px_4px_6px_0px_rgba(0,0,0,0.25)] xl:w-[375px] xl:h-[730px]'>
@@ -34,7 +34,7 @@ export default function WelfareCulture() {
                         <p className='px-5 font-medium leading-[25px] xl:text-xl leading-[30px]'>En este taller vas a aprender a bailar y a disfrutar del folklore. Los invitamos a todos los jóvenes y adultos que deseen conocer esta danza tradicional argentina. No es necesario contar con conocimientos previos, sólo tener ganas y mucho entusiasmo.</p>
                     </div>
                     <div className='flex justify-end py-7 px-5'>
-                        <span className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</span>
+                        <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</a>
                     </div>
                 </div>
                 <div className='Card bg-[#E5B2FF] w-[324px] mb-[30px] rounded-t-lg flex flex-col shadow-[0px_4px_6px_0px_rgba(0,0,0,0.25)] xl:w-[375px] xl:h-[730px]'>
@@ -44,7 +44,7 @@ export default function WelfareCulture() {
                         <p className='px-5 font-medium leading-[25px] xl:text-xl leading-[30px]'>En este taller te invitamos a vivir una experiencia enriquecedora y entretenida, fusionando diversión y aprendizaje a tu propio ritmo, mientras nos sumergimos juntos en el mundo del teatro. Encontrarás un ambiente con un enfoque comprometido y lúdico.</p>
                     </div>
                     <div className='flex justify-end py-7 px-5'>
-                        <span className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</span>
+                        <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</a>
                     </div>
                 </div>
                 <div className='Card bg-[#E5B2FF] w-[324px] mb-[30px] rounded-t-lg flex flex-col shadow-[0px_4px_6px_0px_rgba(0,0,0,0.25)] xl:w-[375px] xl:h-[730px]'>
@@ -54,7 +54,7 @@ export default function WelfareCulture() {
                         <p className='px-5 font-medium leading-[25px] xl:text-xl leading-[30px]'>En este taller podrás reconocer la importancia y beneficios de la organización, reduciendo el estrés y con la posibilidad de realizar mayor cantidad de actividades. El objetivo será que aprendas a balancear la administración sin restar tu bienestar personal.</p>
                     </div>
                     <div className='flex justify-end py-7 px-5'>
-                        <span className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</span>
+                        <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</a>
                     </div>
                 </div>
                 <div className='Card bg-[#E5B2FF] w-[324px] mb-[30px] rounded-t-lg flex flex-col shadow-[0px_4px_6px_0px_rgba(0,0,0,0.25)] xl:w-[375px] xl:h-[730px]'>
@@ -64,7 +64,7 @@ export default function WelfareCulture() {
                         <p className='px-5 font-medium leading-[25px] xl:text-xl leading-[30px]'>En este taller explorarás el concepto y propósito del duelo, mientras adquirís herramientas para mejorar tu inteligencia emocional. Ganarás confianza, paciencia y esperanza mientras transitás tu duelo, encontrando en nosotros un acompañamiento.</p>
                     </div>
                     <div className='flex justify-end py-7 px-5'>
-                        <span className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</span>
+                        <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className='underline text-[#004C0B] text-[17px] font-semibold leading-[25px] xl:text-[22px] xl:leading-[33px]'>Para más info</a>
                     </div>
                 </div>
                 <div className='Card hidden xl:mb-[30px] xl:rounded-t-lg xl:flex xl:flex-col xl:items-center xl:justify-center xl:w-[375px] xl:h-[730px]'>
