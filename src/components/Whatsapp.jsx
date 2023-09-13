2483f8cfcb6e945bc06d3134e788f66b8baa95a2
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-09-13 16:48:44 -0300

    modified whatsapp component styles

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -2,8 +2,8 @@ import whatsapp from "../assets/boton-whatsapp.svg";
 
 export default function Whatsapp() {
   return (
-    <figure className="fixed right-[60px] bottom-[20px] z-[1]">
-      <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className="hover:underline leading-[23px] xl:font-medium xl:leading-[26px]">
+    <figure className="fixed right-[20px] bottom-[20px] z-[1] xl:right-[60px]">
+      <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank">
           <img className="w-[50px] h-[50px]" src={whatsapp} alt="whatsapp" />
       </a>
     </figure>
