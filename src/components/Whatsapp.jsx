d09e80010bc09232da123647729db7c227ff2f20
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-09-13 15:27:00 -0300

    Whatsapp.svg image was added and the icon styles were edited in Whatsapp component

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -1,9 +1,11 @@
-import { RiWhatsappFill } from "react-icons/ri";
+import whatsapp from "../assets/boton-whatsapp.svg";
 
 export default function Whatsapp() {
   return (
-    <figure className="fixed right-[15px] bottom-[15px] z-[1]">
-        <RiWhatsappFill className="w-[24px] h-[24px] "/>
+    <figure className="fixed right-[60px] bottom-[20px] z-[1]">
+      <a href="https://api.whatsapp.com/send?phone=5491165039065" target="_blank" className="hover:underline leading-[23px] xl:font-medium xl:leading-[26px]">
+          <img className="w-[50px] h-[50px]" src={whatsapp} alt="whatsapp" />
+      </a>
     </figure>
   )
 }
